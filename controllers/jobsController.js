7659b8747be40d6c06031b8c8db0a762fbf56860
Jose Leal <lealxh@gmail.com>
2022-01-04 14:46:09 -0300

    job update

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,4 +1,5 @@
 const Job = require("../models/job")
+const geocoder = require("../utils/geocoder")
 
 //get all jobs=> /api/v1/jobs
 exports.getJobs = async (req, res, next) => {
@@ -18,4 +19,48 @@ exports.newJob = async (req, res, next) => {
     message: "Job created",
     data: jobData
   })
+}
+//serch jobs in radius =>/api/v1/jobs/:zipcode/:distance
+module.exports.getJobsInRadius = async (req, res, next) => {
+  const { zipcode, distance } = req.params
+
+  //getting latitude and longitude with geocoder
+  const loc = await geocoder.geocode(zipcode)
+  const latitude = loc[0].latitude
+  const longitude = loc[0].longitude
+
+  const radius = distance / 3963
+
+  const jobs = await Job.find({
+    location: { $geoWithin: { $centerSphere: [[longitude, latitude], radius] } }
+  })
+
+  res.status(200).json({
+    success: true,
+    results: jobs.length,
+    data: jobs
+  })
+}
+//update job by id =>/api/v1/jobs/:id
+module.exports.updateJob = async (req, res, next) => {
+  const { id } = req.params
+
+  let job = await Job.findById(id)
+
+  if (!job)
+    res.status(404).json({
+      success: false,
+      message: "Job not found"
+    })
+  else {
+    job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    })
+    res.status(200).json({
+      success: true,
+      message: "Job updated",
+      data: job
+    })
+  }
 }
--- routes/jobs.js
+++ routes/jobs.js
@@ -2,8 +2,11 @@ const express = require("express")
 const router = express.Router()
 
 //importing jobs controller
-const { getJobs, newJob } = require("../controllers/jobsController")
+const { getJobs, newJob, getJobsInRadius, updateJob } = require("../controllers/jobsController")
+
 router.route("/jobs").get(getJobs)
+router.route("/jobs/:zipcode/:distance").get(getJobsInRadius)
 router.route("/jobs/new").post(newJob)
+router.route("/jobs/:id").put(updateJob)
 
 module.exports = router
