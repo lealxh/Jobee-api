44c32d459d56c41a05d1570d2c7ca96325f03035
Jose Leal <lealxh@gmail.com>
2022-01-05 11:37:14 -0300

    Error handler and error middleware

---
+++ middlewares/Errors.js
@@ -0,0 +1,24 @@
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.message = err.message || "Internal Server Error"
+
+  if (process.env.NODE_ENV == "development") {
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    })
+  }
+
+  if (process.env.NODE_ENV == "production") {
+    let error = { ...err }
+
+    error.message = err.message
+
+    res.status(error.statusCode).json({
+      success: false,
+      message: error.message || "Internal Server Error."
+    })
+  }
+}
