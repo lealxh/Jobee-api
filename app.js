41c868615f1e7e82a1e0393dea7de42626350bb5
Jose Leal <lealxh@gmail.com>
2022-01-06 10:59:40 -0300

    Error handling messages for prod

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require("express")
 const app = express()
 const dotenv = require("dotenv")
 const ErrorMiddleware = require("./middlewares/Errors")
+const ErrorHandler = require("./utils/errorHandler")
 
 //setting up config
 dotenv.config({ path: "./config/config.env" })
 
 const connectDatabase = require("./config/database")
+
+// Handling uncaught Exception
+process.on("uncaughtException", err => {
+  console.log(`Error: ${err.message}`)
+  console.log("Shutting down the server due to uncaught Exception.")
+  server.close(() => {
+    process.exit(1)
+  })
+})
 
 //connection to db
 connectDatabase()
@@ -18,10 +28,23 @@ app.use(express.json())
 const jobs = require("./routes/jobs")
 app.use("/api/v1", jobs)
 
+app.all("*", (req, res, next) => {
+  next(new ErrorHandler(`${req.originalUrl} route not found`, 404))
+})
+
 //midleware to handle errors
 app.use(ErrorMiddleware)
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Process started on port ${process.env.PORT} in ${process.env.NODE_ENV}`)
+})
+
+// Handling Unhandled Promise Rejection
+process.on("unhandledRejection", err => {
+  console.log(`Error: ${err.message}`)
+  console.log("Shutting down the server due to Unhandled promise rejection.")
+  server.close(() => {
+    process.exit(1)
+  })
 })
--- middlewares/Errors.js
+++ middlewares/Errors.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require("../utils/errorHandler")
 
 module.exports = (err, req, res, next) => {
+  console.log(err.message)
   err.statusCode = err.statusCode || 500
   err.message = err.message || "Internal Server Error"
 
@@ -13,7 +14,25 @@ module.exports = (err, req, res, next) => {
     })
   }
   if (process.env.NODE_ENV.trim() == "production") {
-    let error = { ...err }
+    let error = err
+
+    // Wrong Mongoose Object ID Error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid: ${err.path}`
+      error = new ErrorHandler(message, 404)
+    }
+
+    // Handling Mongoose Validation Error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map(value => value.message)
+      error = new ErrorHandler(message, 400)
+    }
+
+    // Handle mongoose duplicate key error
+    if (err.code === 11000) {
+      const message = `Duplicate ${Object.keys(err.keyValue)} entered.`
+      error = new ErrorHandler(message, 400)
+    }
 
     res.status(error.statusCode).json({
       success: false,
