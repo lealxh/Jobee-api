5700d416e3c309792d5eddc55963468df286de9e
Jose Leal <lealxh@gmail.com>
2022-01-10 15:33:38 -0300

    Update and delete user

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,3 +32,39 @@ exports.updatePassword = CatchAsyncErrors(async (req, res, next) => {
 
   sendToken(user, 200, res)
 })
+
+// Update current user data   =>    /api/v1/me/update
+exports.updateUser = CatchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email
+  }
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  })
+
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+})
+
+// Delete current user   =>    /api/v1/me/delete
+exports.deleteUser = CatchAsyncErrors(async (req, res, next) => {
+  // deleteUserData(req.user.id, req.user.role);
+
+  const user = await User.findByIdAndDelete(req.user.id)
+
+  res.cookie("token", "none", {
+    expires: new Date(Date.now()),
+    httpOnly: true
+  })
+
+  res.status(200).json({
+    success: true,
+    message: "Your account has been deleted."
+  })
+})
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-const { getUserProfile, updatePassword } = require("../controllers/userController")
+const { getUserProfile, updatePassword, updateUser, deleteUser } = require("../controllers/userController")
 
 const { isAuthenticatedUser } = require("../middlewares/auth")
 
 router.route("/me").get(isAuthenticatedUser, getUserProfile)
 router.route("/password/change").put(isAuthenticatedUser, updatePassword)
-
+router.route("/me/update").put(isAuthenticatedUser, updateUser)
+router.route("/me/delete").delete(isAuthenticatedUser, deleteUser)
 module.exports = router
